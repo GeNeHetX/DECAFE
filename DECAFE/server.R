a0105e1d213b1ccdf246ba9c930e2c8c8a626fbc
Audrey Beaufils <audrey_beaufils@hotmail.fr>
2024-05-03 11:05:47 +0200

    Update server.R

--- DECAFE/server.R
+++ DECAFE/server.R
@@ -694,7 +694,7 @@ pca_alldownload <- reactive({
           geom_vline(xintercept=0, linetype="dashed", color = "grey") +
           geom_hline(yintercept=0, linetype="dashed", color = "grey")
 
-        ggsave(file, plot, width = 8, height = 6, units = "in", dpi = 300)
+        ggsave(file, plot, width = 8, height = 6, units = "in", dpi = 300, device = input$format)
       })
 
 
