afae8097f71ab9e2958095f25460581884f57570
Audrey Beaufils <audrey_beaufils@hotmail.fr>
2024-07-29 17:53:26 +0200

    Update ui.R

--- DECAFE/ui.R
+++ DECAFE/ui.R
@@ -338,7 +338,8 @@ library(shinyBS)
             column(width =3,
               uiOutput('cond1'),
               uiOutput('cond2'),
-              p(icon('circle-info'),paste0("Group 1 is considered as control \n UP represented upregulated in group 2 "))
+              p(icon('circle-info'),paste0("Group 1 is considered as control  ")),
+              p(paste0("UP represented upregulated in group 2 compared to group 1"))
                    
             )
 
